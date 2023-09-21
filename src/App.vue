96947b3a3731111712ff2779a7da55b8ff77cf72
Will Luna <willluna.zac.com@gmail.com>
2023-09-21 14:41:31 -0500

    Curso de Vue.js: Componentes y Composition API:Transicions

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="show = !show">Menu</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
+</template>
+
+<script>
+import Menu from "./components/Menu.vue";
+
+export default {
+  name: "App",
+  components: { Menu },
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
+}
+</style>
