1357d73201c59c3bbfb852d1c658260b284eef6b
Will Luna <willluna.zac.com@gmail.com>
2023-09-27 12:05:50 -0500

    Curso de Vue.js: Componentes y Composition API:Composition api vs option api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <HomeVue name="Will" last-name="Luna"></HomeVue>
+  <HomeOptions name="Will" last-name="Luna"></HomeOptions>
 </template>
 
 <script>
 import HomeVue from "./components/HomeVue.vue";
 import { provide } from "vue";
+import HomeOptions from "./components/HomeOptions.vue";
 
 export default {
   name: "App",
   components: {
     HomeVue,
+    HomeOptions,
   },
   setup() {
     provide("userName", "WillPlayer");
---
+++ src/components/HomeOptions.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ fullName }}</div>
+  <div>{{ userName }}</div>
+  <button ref="btn">Click!</button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    lastName: String,
+  },
+  inject: ["userName"],
+  computed: {
+    fullName() {
+      return `${this.name} ${this.lastName}`;
+    },
+  },
+  mounted() {
+    console.log(this.$refs.btn);
+  },
+};
+</script>
